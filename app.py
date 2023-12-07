ad35edcd48ad96891042619699c6f1a81c7d5a96
Chernenko <pichernenko@gmail.com>
2023-12-07 14:00:15 +0700

    major update

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ init_state(init_sessions=['cur_kamar_increment'
                           ,'cur_tagihan_rs'
                           ,'cur_jumlah_hari'
                           ,'%_moc_paid'
-                          ,'%_self_paid'])
+                          ,'%_self_paid'
+                          ,'max_harga_kamar'])
 
 with st.sidebar:
     st.title("[Asuransimurni.com](https://Asuransimurni.com) Tools")
     st.write("Membantu nasabah dalam mendapatkan informasi terkait Klaim maupun Administrasi terkait Asuransi AXA Financial Indonesia.")
     st.selectbox("Menu",['Upgrade Downgrade Kamar','Download Area'],key='menu')
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.write("[Website](https://asuransimurni.com)")
+    with col2:
+        st.write("[Linkedin](https://www.linkedin.com/in/nencor)")
+    with col3:
+        st.write("[Twitter](https://twitter.com/asmurcom)")
 
 tab1,tab2 = st.tabs(['Home','Debug'])
 
@@ -20,16 +28,18 @@ with tab1:
     if st.session_state['menu'] == 'Upgrade Downgrade Kamar':
             st.subheader("Perhitungan Upgrade Downgrade Kamar [Maestro Optima Care](https://asuransimurni.com/produk/asuransi-kesehatan/maestro-optima-care/)")
             st.write("Menu ini bertujuan untuk menghitung estimasi biaya tagihan Rumah Sakit yang excess diakibatkan oleh upgrade kamar.")
-            st.write("Saat ini hanya berlaku khusus nasabah Maestro Optima Care dengan Plan Emerald.")
-            st.selectbox("Plan saat ini",["Emerald"],key='cur_plan')
-            st.number_input("Masukkan harga per malam kamar rawat inap 1 bed per room termurah di Rumah Sakit",min_value=0,key='cur_kamar_rs',on_change=calc_kamar)
+            st.selectbox("Plan saat ini",config_kamar.keys(),index=2,key='cur_plan')
+            st.selectbox("Berobat di",['Indonesia','Luar Indonesia'],index=0,key='cur_negara')
+            st.number_input("Masukkan harga per malam kamar rawat inap {} bed per room termurah di Rumah Sakit".format(config_kamar[st.session_state['cur_plan']]['jumlah_bed']),min_value=0,key='cur_kamar_rs',on_change=calc_kamar)
+            if st.session_state['cur_kamar_rs']:
+                st.write("Harga Kamar yang akan diperhitungkan adalah {:,}".format(st.session_state['max_harga_kamar']))
             st.number_input("Masukkan harga kamar per malam yang dituju",min_value=0,key='cur_kamar_tujuan_rs',on_change=calc_kamar)
             
             if st.session_state['cur_kamar_rs']>0 and st.session_state['cur_kamar_tujuan_rs']:
-                if st.session_state['cur_kamar_increment']<=0.2:
+                if st.session_state['cur_kamar_increment']<=0.2 and st.session_state['cur_negara']=='Indonesia':
                     st.number_input("Estimasi jumlah hari dirawat",min_value=0,key='cur_jumlah_hari',on_change=calc_kamar)
                     if st.session_state['cur_jumlah_hari']>0:
-                        st.write("Total harga kamar: {:,} ".format(st.session_state['cur_kamar_rs']))
+                        st.write("Total harga kamar: {:,} ".format(st.session_state['max_harga_kamar']))
                         st.write("Total harga kamar yang dituju: {:,}".format(st.session_state['cur_kamar_tujuan_rs']))
                         st.write(":exclamation: Selisih harga kamar: {:,}".format(st.session_state['selisih_kamar']))
                         st.write("Total hari dirawat: {} hari".format(st.session_state['cur_jumlah_hari']))
@@ -38,9 +48,9 @@ with tab1:
                                                                                                         ,st.session_state['total_billing_subsidi']
                                                                                                         ))
                 else:
-                    st.write("Harga kamar tujuan ialah {:,} dan kenaikan harga kamar sama dengan {}% dari harga kamar asal {:,}, maka akan dikenakan perhitungan prorata dari total tagihan Rumah Sakit".format(st.session_state['cur_kamar_tujuan_rs']
+                    st.write("Harga kamar tujuan ialah {:,} dan kenaikan harga kamar sama dengan {}% dari harga kamar yang diperhitungkan {:,}, maka akan dikenakan perhitungan prorata dari total tagihan Rumah Sakit".format(st.session_state['cur_kamar_tujuan_rs']
                                                                                                         ,round(st.session_state['cur_kamar_increment']*100)
-                                                                                                        ,st.session_state['cur_kamar_rs']))
+                                                                                                        ,st.session_state['max_harga_kamar']))
                     st.number_input("Masukkan estimasi total Billing Rumah Sakit",min_value=0,help="Angka ini dapat dikonfirmasi melalui bagian Admisi / Asuransi tiap Rumah Sakit",key='cur_tagihan_rs',on_change=calc_kamar)
                     if st.session_state['cur_tagihan_rs']>0:
                         st.write("Estimasi Total Tagihan Rumah Sakit: {:,} (100%)".format(st.session_state['cur_tagihan_rs']))
--- backend.py
+++ backend.py
@@ -1,19 +1,47 @@
 import streamlit as st
 
+config_kamar = {
+    'Opal':{
+        'harga_kamar':500000,
+        'jumlah_bed':2
+    },
+    'Emerald Blue':{
+        'harga_kamar':500000,
+        'jumlah_bed':2
+    },
+    'Emerald':{
+        'harga_kamar':1000000,
+        'jumlah_bed':1
+    },
+    'Ruby Blue':{
+        'harga_kamar':500000,
+        'jumlah_bed':2
+    },
+    'Ruby':{
+        'harga_kamar':1200000,
+        'jumlah_bed':1
+    },
+    'Diamond':{
+        'harga_kamar':1500000,
+        'jumlah_bed':1
+    }
+}
+
 def init_state(init_sessions):
     for session in init_sessions:
         if session not in st.session_state:
             st.session_state[session]=0
 
 def calc_kamar():
-    st.session_state['cur_kamar_increment'] = (st.session_state['cur_kamar_tujuan_rs'] - st.session_state['cur_kamar_rs'])/st.session_state['cur_kamar_rs']
+    st.session_state['max_harga_kamar'] = st.session_state['cur_kamar_rs'] if st.session_state['cur_kamar_rs'] >= config_kamar[st.session_state['cur_plan']]['harga_kamar'] else config_kamar[st.session_state['cur_plan']]['harga_kamar']
+    st.session_state['cur_kamar_increment'] = (st.session_state['cur_kamar_tujuan_rs'] - st.session_state['max_harga_kamar'])/st.session_state['max_harga_kamar']
     #calc subsidi:
     if st.session_state['cur_jumlah_hari']>0:
-        st.session_state['selisih_kamar'] = st.session_state['cur_kamar_tujuan_rs']-st.session_state['cur_kamar_rs']
+        st.session_state['selisih_kamar'] = st.session_state['cur_kamar_tujuan_rs']-st.session_state['max_harga_kamar']
         st.session_state['total_billing_subsidi'] = st.session_state['selisih_kamar'] * st.session_state['cur_jumlah_hari']
     #calc prorata
     if st.session_state['cur_tagihan_rs']>0:
-        st.session_state['cur_tagihan_self_paid'] = (st.session_state['cur_kamar_rs']*st.session_state['cur_tagihan_rs'])/st.session_state['cur_kamar_tujuan_rs']
+        st.session_state['cur_tagihan_self_paid'] = int((st.session_state['cur_kamar_rs']*st.session_state['cur_tagihan_rs'])/st.session_state['cur_kamar_tujuan_rs'])
         st.session_state['%_self_paid'] = int((st.session_state['cur_tagihan_self_paid']/st.session_state['cur_tagihan_rs'])*100)
-        st.session_state['cur_tagihan_rs_moc_paid'] = st.session_state['cur_tagihan_rs'] - st.session_state['cur_tagihan_self_paid']
+        st.session_state['cur_tagihan_rs_moc_paid'] = int(st.session_state['cur_tagihan_rs'] - st.session_state['cur_tagihan_self_paid'])
         st.session_state['%_moc_paid'] = int((st.session_state['cur_tagihan_rs_moc_paid']/st.session_state['cur_tagihan_rs'])*100)

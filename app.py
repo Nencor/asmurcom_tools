ac91b96c08bd1fe25efbacc04a3e59ff0746cfad
Chernenko <pichernenko@gmail.com>
2023-12-08 13:27:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ init_state(init_sessions=['cur_kamar_increment'
                           ,'%_self_paid'
                           ,'max_harga_kamar'
                           ,'eligible_moc_score'])
-init_state(['check_eligible_usia','check_eligible_penyakit','check_eligible_rutin'],valuenya=False)
+init_state(['check_eligible_usia','check_eligible_penyakit','check_eligible_rutin'],valuenya=True)
 
 menunya = ['Upgrade Downgrade Kamar'
            ,'Download Formulir Klaim'

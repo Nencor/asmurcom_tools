69a3d1ce4baccf721c6f4fbc496422d5bc8dc7fa
Chernenko <pichernenko@gmail.com>
2023-12-11 14:53:51 +0700

    update
    develop fitur kroscek klaim reimbursement

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import streamlit as st 
 from backend import * 
 
-init_state(init_sessions=['cur_kamar_increment'
-                          ,'cur_tagihan_rs_moc_paid'
-                          ,'cur_tagihan_self_paid'
-                          ,'cur_tagihan_rs'
-                          ,'cur_jumlah_hari'
-                          ,'%_moc_paid'
-                          ,'%_self_paid'
-                          ,'max_harga_kamar'
-                          ,'eligible_moc_score'])
-init_state(['check_eligible_usia','check_eligible_penyakit','check_eligible_rutin'],valuenya=True)
+reset_state()
 
 linknya = {
     'moc':'https://asuransimurni.com/produk/asuransi-kesehatan/maestro-optima-care/',
@@ -18,12 +9,68 @@ linknya = {
     'linkedin':'https://www.linkedin.com/in/nencor',
     'twitter':'https://twitter.com/asmurcom',
     'looker':'https://lookerstudio.google.com/reporting/1687e67d-6dbe-4bac-bacd-988cf433bebe/page/NpqcD',
-    'foto':'https://pbs.twimg.com/profile_images/1638728562814640129/s902CLPl_400x400.jpg'
+    'foto':'https://pbs.twimg.com/profile_images/1638728562814640129/s902CLPl_400x400.jpg',
+    'alamat_klaim':"""STO Telkom Gambir
+Lantai 3, Jl. Medan Merdeka Sel. No.12, RT.11/RW.2
+Gambir, Kecamatan Gambir, Kota Jakarta Pusat, 
+Daerah Khusus Ibukota Jakarta 10110""",
+    'whatsapp':'https://wa.me/6281210429617'
 }
+
+checkbox_kurir = ['JNE','JNT','TIKI','POS Indonesia','SAP','SiCepat','JET']
 menunya = ['Upgrade Downgrade Kamar'
         ,'Download Formulir Klaim'
         ,'Pertanyaan Kesehatan MOC'
         ,'Klaim Reimbursement MOC']
+
+kelengkapan_klaim = {
+    'suket_dokter':{
+        'name':'[Surat Keterangan Dokter]({})'.format(linknya['looker']),
+        'key':'kelengkapan_klaim_suket_dokter',
+        'help':'Surat Keterangan Dokter wajib diisi oleh dokter dan disign oleh dokter serta stempel Rumah Sakit'
+    },
+    'invoice':{
+        'name':'Invoice / Tagihan Rumah Sakit',
+        'key':'kelengkapan_klaim_invoice',
+        'help':None
+    },
+    'kwitansi':{
+        'name':'Kwitansi / Official Receipt / Bukti Bayar',
+        'key':'kelengkapan_klaim_kwitansi',
+        'help':None
+    },
+    'tax_invoice':{
+        'name':'Tax Invoice (bila ada. Bila tidak ada maka tetap diceklis)',
+        'key':'kelengkapan_klaim_tax_invoice',
+        'help':'Pembayaran pajak dapat ditagihkan ke Penanggung'
+    },
+    'resume_medis':{
+        'name':'Resume Medis / Discharge Summary',
+        'key':'kelengkapan_klaim_resume_medis',
+        'help':"Dapat diminta ke bagian Admisi Rumah Sakit."
+    },
+    'billing_details':{
+        'name':'Rincian biaya, perawatan, dan obat-obatan selama Rawat Inap',
+        'key':'kelengkapan_klaim_billing_details',
+        'help':"Dapat diminta ke bagian Admisi Rumah Sakit"
+    },
+    'hasil_lab':{
+        'name':'Hasil Laboratorium dan analisa dokter (baca rontgen, MRI,CT Scan, dll)',
+        'key':'kelengkapan_klaim_hasil_lab',
+        'help':"Dapat diminta ke bagian Admisi Rumah Sakit"
+    }
+    ,'ktp':{
+        'name':'Copy KTP Tertanggung',
+        'key':'kelengkapan_klaim_ktp_tertanggung',
+        'help':"Untuk keperluan verifikasi data TErtanggung"
+    },
+    'rekening':{
+        'name':'Copy Buku Rekening Tabungan untuk pembayaran klaim',
+        'key':'kelengkapan_klaim_buku_rekening',
+        'help':"Masukkan nomor rekening yang masih aktif"
+    }
+
+}
 
 
 penyakit_eligible_moc = [
@@ -50,6 +97,7 @@ with st.sidebar:
     st.title("[Asuransimurni.com](https://Asuransimurni.com) Tools")
     st.write("Membantu nasabah dalam mendapatkan informasi terkait Klaim maupun Administrasi terkait Asuransi AXA Financial Indonesia.")
     st.selectbox("Menu",menunya,key='menu')
+    st.write(f"[Butuh bantuan?]({linknya['whatsapp']})")
 
 tab1,tab2 = st.tabs(['Home','Debug'])
 
@@ -103,8 +151,33 @@ with tab1:
     elif st.session_state['menu'] == 'Klaim Reimbursement MOC':
         st.subheader("Klaim Reimbursement [Maestro Optima Care]({})".format(linknya['moc']))
         st.write("Menu ini bertujuan untuk melakukan verifikasi apakah dokumen Klaim sudah lengkap sebelum dikirim")
-        st.write("Coming soon")
-    else:
-        st.subheader("Coming soon.")
+        st.write("Mohon untuk melengkapi dokumen di bawah ini:")
+        
+        st.session_state['kelengkapan_score']=0
+        
+        for item in kelengkapan_klaim:
+            st.checkbox(kelengkapan_klaim[item]['name']
+                        ,key=kelengkapan_klaim[item]['key']
+                        ,help=kelengkapan_klaim[item]['help']
+                        ,on_change=calc_kelengkapan_klaim)        
+        my_bar = st.progress(st.session_state['progress_value'], text="Kelengkapan Dokumen")
+        if st.session_state['progress_value']==1:
+            st.success("Terima kasih. Dokumen telah lengkap. Mohon untuk mengirimkan seluruh berkas dan dokumen untuk keperluan Klaim via ekspedisi yang memiliki nomor resi seperti JNE, JNT, TIKI, dsb ke alamat di bawah ini:")
+            st.code(linknya['alamat_klaim'])
+            st.checkbox("Saya telah mengirimkan dokumen Klaim ke alamat tersebut",key="klaim_sent")
+            if st.session_state['klaim_sent']:
+                col1,col2,col3 = st.columns(3)
+                with col1:
+                    st.text_input("Nomor Polis",key='klaim_no_polis',help="cth: 570-9999999")
+                with col2:
+                    st.selectbox("Pilih Kurir / Jasa Ekspedisi",checkbox_kurir,index=0,key='klaim_kurir',on_change=calc_kelengkapan_klaim)
+                with col3:
+                    st.text_input("Nomor Resi",key='klaim_resi')
+
+                if st.session_state['klaim_no_polis'] and st.session_state['klaim_sent'] and st.session_state['klaim_resi']:
+                    st.markdown(f"[Kirim Nomor Resi via Whatsapp](https://api.whatsapp.com/send/?phone={linknya['whatsapp']}&text=dokumen%20klaim%20nomor%20polis%20{st.session_state['klaim_no_polis']}%20telah%20dikirim%20menggunakan%20{str(st.session_state['klaim_kurir']).replace(' ','%20') }%20dengan%20nomor%20resi%20{st.session_state['klaim_resi']}&type=phone_number&app_absent=0)")
+                else:
+                    st.error("Lengkapi Nomor Polis / Nomor Resi terlebih dahulu untuk mengirimkan nomor resi pengiriman via Whatsapp.")
+                    
 with tab2:
     st.session_state
--- backend.py
+++ backend.py
@@ -32,6 +32,24 @@ def init_state(init_sessions,valuenya=0):
         if session not in st.session_state:
             st.session_state[session]=valuenya
 
+def reset_state():
+    init_state(init_sessions=['cur_kamar_increment'
+                          ,'cur_tagihan_rs_moc_paid'
+                          ,'cur_tagihan_self_paid'
+                          ,'cur_tagihan_rs'
+                          ,'cur_jumlah_hari'
+                          ,'%_moc_paid'
+                          ,'%_self_paid'
+                          ,'max_harga_kamar'
+                          ,'eligible_moc_score'
+                          ,'kelengkapan_score'
+                          ,'progress_value'
+                          ,'klaim_no_polis'
+                          ,'klaim_kurir'])
+init_state(['check_eligible_usia'
+            ,'check_eligible_penyakit'
+            ,'check_eligible_rutin'],valuenya=True)
+
 def calc_kamar():
     st.session_state['max_harga_kamar'] = st.session_state['cur_kamar_rs'] if st.session_state['cur_kamar_rs'] >= config_kamar[st.session_state['cur_plan']]['harga_kamar'] else config_kamar[st.session_state['cur_plan']]['harga_kamar']
     st.session_state['cur_kamar_increment'] = (st.session_state['cur_kamar_tujuan_rs'] - st.session_state['max_harga_kamar'])/st.session_state['max_harga_kamar']
@@ -45,11 +63,20 @@ def calc_kamar():
         st.session_state['%_self_paid'] = int((st.session_state['cur_tagihan_self_paid']/st.session_state['cur_tagihan_rs'])*100)
         st.session_state['cur_tagihan_rs_moc_paid'] = int(st.session_state['cur_tagihan_rs'] - st.session_state['cur_tagihan_self_paid'])
         st.session_state['%_moc_paid'] = int((st.session_state['cur_tagihan_rs_moc_paid']/st.session_state['cur_tagihan_rs'])*100)
+
 def calc_eligible_moc():
-
     init_state(['check_eligible_usia','check_eligible_penyakit','check_eligible_rutin'],valuenya=False)
-
     st.session_state['check_eligible_usia'] = True if st.session_state['eligible_usia']<=80 else False
     st.session_state['check_eligible_penyakit'] = True if st.session_state['eligible_penyakit'] =='Tidak ada' else False
     st.session_state['check_eligible_rutin'] = True if st.session_state['eligible_rutin']=='Tidak' else False
 
+def calc_kelengkapan_klaim():
+    count_true = 0 #to have it presented in st.progress
+    count_checkbox=0 # count_true / count_checkbox
+    for item in st.session_state:        
+        if 'kelengkapan_klaim' in item: #this is where st.checkbox is
+            count_checkbox+=1
+            if st.session_state[item] is True:
+                count_true+=1 
+    st.session_state['progress_value'] = count_true/count_checkbox
+    print("Klaim Kurir is {}".format(str(st.session_state['klaim_kurir']).replace(" ","%20")))
